d6ee6bffad8a1e5c7bb8447aa399906e5a7f132c
EdKazik <edoskazik@gmail.com>
2023-07-03 14:22:22 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+$("#btn").click(function (event) {
+    console.log('ERRor', event.target);
+    $(this).prop('disabled', true);
+    const newDiv = $("<div id='new_div'></div>").css({ "height": '100px', 'width': '200px', 'border': '8px solid blue', 'background': 'yellow'})
+    $(this).before(newDiv);
+    const newBtn = $("<button id='new_btn'>Close</button>").css({ 'margin-left': '80px', 'margin-top': '35px'});
+    newDiv.append(newBtn);
+    $(new_btn).click(() => {
+        $(this).prop('disabled', false);
+        newDiv.remove()
+    })
+});
+
